e75a88ebf6ec736a9ddd89b58d497ec5cd091b40
kevinluc95 <kevin.luc@shopback.com>
2020-11-21 10:40:57 +0700

    update

--- src/crawl_data.js
+++ src/crawl_data.js
@@ -1,21 +1,28 @@
 const puppeteer = require("puppeteer");
+const XLSX = require('xlsx')
 const Utils = require("./ultils");
 
 (async () => {
-    const wsChromeEndpointUrl = 'ws://127.0.0.1:9222/devtools/browser/8916b773-6a93-4708-8e63-df958cbc57ff';
+    const wsChromeEndpointUrl = 'ws://127.0.0.1:9222/devtools/browser/9799ab1d-f347-476c-bebf-644d7223f9a6';
     const browser = await puppeteer.connect({
         browserWSEndpoint: wsChromeEndpointUrl,
     });
     const page = await browser.newPage();
+    await page.setViewport({ width: 1366, height: 768 });
     await page.goto("https://wefinex.net/index", {
         waitUntil: 'networkidle0'
     });
-    await page.setViewport({ width: 1366, height: 768 });
     await page.waitForSelector('path.highcharts-point');
+    const total_candle = await page.$$('path.highcharts-point')
     let result = []
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i <= total_candle.length; i++) {
+        await page.waitForSelector(`path.highcharts-point:nth-child(${i})`);
         await page.click(`path.highcharts-point:nth-child(${i})`)
-        const candleResult = await Utils.getValuePerCandle(page, 5);
+        const candleResult = await Utils.getValuePerCandle(page, i);
         result.push(candleResult)
     }
+    const data = XLSX.utils.json_to_sheet(result);
+    const workbook = XLSX.utils.book_new() 
+    XLSX.utils.book_append_sheet(workbook, data) 
+    XLSX.writeFile(workbook, 'data.xlsx');
 })();
--- src/ultils.js
+++ src/ultils.js
@@ -1,16 +1,45 @@
+
 class Utils {
+    dataFormater(text){
+        const data = text.split(":")
+        return data[1]
+    }
+
     async getValuePerCandle(page, candleIndex) {
-        const O_VALUE_LOCATOR = await page.$x(`//*[@class='highcharts-point highcharts-point-down'][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[1]/span[1]`);
-        const C_VALUE_LOCATOR = await page.$x(`//*[@class='highcharts-point highcharts-point-down'][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[2]`);
-        const H_VALUE_LOCATOR = await page.$x(`//*[@class='highcharts-point highcharts-point-down'][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[4]`);
-        const L_VALUE_LOCATOR = await page.$x(`//*[@class='highcharts-point highcharts-point-down'][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[5]`);
-        const VOL_VALUE_LOCATOR = await page.$x(`//*[@class='highcharts-point highcharts-point-down'][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[6]`);
-        let O_Result = await page.evaluate((el) => { return el.textContent }, O_VALUE_LOCATOR[0]);
-        let C_Result = await page.evaluate((el) => { return el.textContent }, C_VALUE_LOCATOR[0]);
-        let H_Result = await page.evaluate((el) => { return el.textContent }, H_VALUE_LOCATOR[0]);
-        let L_Result = await page.evaluate((el) => { return el.textContent }, L_VALUE_LOCATOR[0]);
-        let Vol_Result = await page.evaluate((el) => { return el.textContent }, VOL_VALUE_LOCATOR[0]);
+        let datetime = new Date().toLocaleString();
+        let O_Result;
+        let C_Result;
+        let H_Result;
+        let L_Result;
+        let Vol_Result;
+        const O_VALUE_LOCATOR = await page.$x(`//*[contains(@class, 'highcharts-candlestick-series')]//*[contains(@class,'highcharts-point')][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[1]/span[1]`);
+        const C_VALUE_LOCATOR = await page.$x(`//*[contains(@class, 'highcharts-candlestick-series')]//*[contains(@class,'highcharts-point')][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[2]`);
+        const H_VALUE_LOCATOR = await page.$x(`//*[contains(@class, 'highcharts-candlestick-series')]//*[contains(@class,'highcharts-point')][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[4]`);
+        const L_VALUE_LOCATOR = await page.$x(`//*[contains(@class, 'highcharts-candlestick-series')]//*[contains(@class,'highcharts-point')][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[5]`);
+        const VOL_VALUE_LOCATOR = await page.$x(`//*[contains(@class, 'highcharts-candlestick-series')]//*[contains(@class,'highcharts-point')][${candleIndex}]//ancestor::div[@class='highcharts-container ']//div[contains(@class, 'highcharts-tooltip')]//span[6]`);
+        if (O_VALUE_LOCATOR.length >= 1) {
+            let O_Data = await page.evaluate((el) => { return el.textContent }, O_VALUE_LOCATOR[0]);
+            O_Result = this.dataFormater(O_Data)
+        }
+        if (C_VALUE_LOCATOR.length >= 1) {
+            let C_Data = await page.evaluate((el) => { return el.textContent }, C_VALUE_LOCATOR[0]);
+            C_Result = this.dataFormater(C_Data)
+        }
+        if (H_VALUE_LOCATOR.length >= 1) {
+            let H_Data = await page.evaluate((el) => { return el.textContent }, H_VALUE_LOCATOR[0]);
+            H_Result = this.dataFormater(H_Data)
+        }
+        if (L_VALUE_LOCATOR.length >= 1) {
+            let L_Data = await page.evaluate((el) => { return el.textContent }, L_VALUE_LOCATOR[0]);
+            L_Result = this.dataFormater(L_Data)
+        }
+        if (VOL_VALUE_LOCATOR.length >= 1) {
+            let Vol_Data = await page.evaluate((el) => { return el.textContent }, VOL_VALUE_LOCATOR[0]);
+            Vol_Result = this.dataFormater(Vol_Data)
+        }
         return {
+            index: candleIndex,
+            datetime: datetime,
             O: O_Result,
             C: C_Result,
             H: H_Result,
